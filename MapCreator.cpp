cfd786e1c5d4685ad6225644c50c13a1cb461858
Dima <dimka11lp@gmail.com>
2018-04-26 23:42:15 +0600

    text strings

--- MapCreator.cpp
+++ MapCreator.cpp
@@ -13,7 +13,27 @@ MapGameField& MapCreator::getMap()
 	place0_text.push_back("Вы победили, теперь вы можете выбрать место для дальнейшего продвижения");
 	place0_text.push_back("Вы проиграли!");
 
+	vector<string> place1_text;
+	place1_text.push_back("Вы пришли в новое место");
+
+	vector<string> place2_text;
+	place2_text.push_back("");
+
+	vector<string> place3_text;
+	place3_text.push_back("");
+
+	vector<string> place4_text;
+	place4_text.push_back("");
+
+	vector<string> place5_text;
+	place5_text.push_back("Это последнее место");
+
 	mgf->addPlace(0, 0, "Start !", { 1,2 }, place0_text);
+	mgf->addPlace(0, 0, "Start !", { 4,2 }, place1_text);
+	mgf->addPlace(0, 0, "Start !", { 1,3 }, place2_text);
+	mgf->addPlace(0, 0, "Start !", { 4,5 }, place3_text);
+	mgf->addPlace(0, 0, "Start !", { 3,5 }, place4_text);
+	mgf->addPlace(0, 0, "Start !", { 4,5 }, place5_text);
 
 	return *mgf;
 }
