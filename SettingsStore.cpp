8871fcf9089d628b7fb3a4c947da0eb6a889bf9a
Dima <dimka11lp@gmail.com>
2018-05-03 23:20:15 +0600

    settings / state object

--- SettingsStore.cpp
+++ SettingsStore.cpp
@@ -1,11 +1,33 @@
 #include "SettingsStore.h"
 
-
-SettingsStore::SettingsStore()
+void SettingsPlayer::Set(vector<int> pls)
 {
+	Health = pls.at(0);
+	Manna = pls.at(1);
 }
 
-
-SettingsStore::~SettingsStore()
+vector<int> SettingsPlayer::Get()
 {
+	return { Health, Manna };
+}
+
+void SettingsEnemy::Set(vector<int> ens)
+{
+	Health = ens.at(0);
+}
+
+vector<int> SettingsEnemy::Get()
+{
+	return { Health };
+}
+
+void SettingsGame::Set(vector<int> sg)
+{
+	currentPlaceOnMap = sg.at(0);
+	currentTextStringShow = sg.at(1);
+}
+
+vector<int> SettingsGame::Get()
+{
+	return { currentPlaceOnMap, currentTextStringShow };
 }
--- SettingsStore.h
+++ SettingsStore.h
@@ -1,23 +1,37 @@
 #ifndef SettingsStore_H
 #define SettingsStore_H
-
-class SettingsStore //todo классы игры игрока противника будут вызывать методы настроек?
+#include <vector>
+using namespace std;
+class SettingsStore
 {
 public:
-	SettingsStore(); // Вызов методов класса загрузки настроек
-	~SettingsStore(); // Вызов методов класса сохранения настроек
+	SettingsStore() = default;
+	~SettingsStore() = default;
+	virtual vector<int> Get() = 0;
+	virtual void Set(vector<int>) = 0;
 };
 
 class SettingsPlayer : SettingsStore{
-
+	int Health;
+	int Manna;
+public:
+	void Set(vector<int>) override;
+	vector<int> Get() override;
 };
 
 class SettingsEnemy : SettingsStore{
-
+	int Health;
+public:
+	void Set(vector<int>) override;
+	vector<int> Get() override;
 };
 
 class SettingsGame : SettingsStore{
-
+	int currentPlaceOnMap;
+	int currentTextStringShow;
+public:
+	void Set(vector<int>) override;
+	vector<int> Get() override;
 };
 
 #endif SettingsStore_H
